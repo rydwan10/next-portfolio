bc2c69c7ae4b004f43a0f2711406923cc66f2ead
Muhammad Rydwan <rydwan.dev@gmail.com>
2023-07-02 03:30:53 +0700

    remove unsued experience.tsx component

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import textStyle from "./utils/styles/Text.module.css";
 import Spacer from "./utils/spacer";
-import ExperienceCard from "./utils/experience_card";
+
 import { data } from "./data/data";
 
 import {
@@ -19,16 +19,6 @@ const Experience: React.FC = () => {
             </Typography>
             <Spacer height={100} />
             <CustomTimeline experiences={data} />
-            {/* {
-                data.map((datum) => {
-                    return (
-                        <div key={datum.id} className="flex content-start">
-                            <ExperienceCard key={datum.id} year={datum.year} title={datum.title} subTitle={datum.subTitle} experiences={datum.experiences} id={datum.id} />
-                        </div>
-                    )
-                })
-
-            } */}
         </div>
     )
 }
