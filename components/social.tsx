4739a7c45f3369c7729107e0b07e358f547c64a0
Muhammad Rydwan <rydwan.dev@gmail.com>
2024-07-01 15:53:33 +0700

    update cv 2

--- components/social.tsx
+++ components/social.tsx
@@ -36,7 +36,7 @@ const Social: React.FC = () => {
                 />
             </a>
 
-            <a href="/assets/pdf/rydwan_cv_2024.pdf" target="_blank" rel="noopener noreferrer" className="rounded-full border-2 border-purple-500 px-4 py-2 flex items-center space-x-2 text-white">
+            <a href="/assets/pdf/MuhammadRydwanSoftwareEnginner.pdf" target="_blank" rel="noopener noreferrer" className="rounded-full border-2 border-purple-500 px-4 py-2 flex items-center space-x-2 text-white">
                 <span className="mr-2 font-semibold">Download CV</span>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
